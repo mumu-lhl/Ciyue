2099cee7c10160e3bfe70250db7be86f85931cac
Mumulhl <mumulhl.666@gmail.com>
2025-02-03 20:15:05 +0800

    feat: add more options button switch

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,7 @@ void main() async {
     "dictionariesDirectory",
     "exportPath",
     "notification",
+    "showMoreOptionsButton",
   }));
 
   int? groupId = prefs.getInt("currentDictionaryGroupId");
