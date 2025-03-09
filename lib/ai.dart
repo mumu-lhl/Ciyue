ff31cf529889e7b538e9feead2cd3b43b1d2efe1
Mumulhl <mumulhl.666@gmail.com>
2025-03-09 11:59:44 +0800

    feat: implement AI provider classes for OpenAI and Gemini APIs

---
+++ lib/ai.dart
@@ -0,0 +1,122 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+
+class AI {
+  final String provider;
+  final String model;
+  final String apikey;
+  late final AIProvider aiProvider;
+
+  AI({
+    required this.provider,
+    required this.model,
+    required this.apikey,
+  }) {
+    if (provider == 'openai') {
+      aiProvider = OpenAIProvider(apikey: apikey, model: model);
+    } else if (provider == 'gemini') {
+      aiProvider = GeminiProvider(apikey: apikey, model: model);
+    }
+  }
+
+  Future<String> _request(String prompt) async {
+    return aiProvider.request(prompt);
+  }
+}
+
+abstract class AIProvider {
+  Future<String> request(String prompt);
+}
+
+class GeminiProvider extends AIProvider {
+  GeminiProvider({required this.apikey, required this.model});
+
+  final String apikey;
+  final String model;
+
+  static const String apiUrl =
+      "https://generativelanguage.googleapis.com/v1beta/models/{model}:generateContent";
+
+  @override
+  Future<String> request(String prompt) async {
+    final dio = Dio();
+    final formattedApiUrl = apiUrl.replaceFirst('{model}', model);
+
+    final params = {'key': apikey};
+
+    final headers = {
+      'Content-Type': 'application/json',
+    };
+    final data = {
+      "contents": [
+        {
+          "parts": [
+            {"text": prompt}
+          ]
+        }
+      ]
+    };
+
+    try {
+      final response = await dio.post(
+        formattedApiUrl,
+        queryParameters: params,
+        options: Options(headers: headers),
+        data: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        final decodedResponse = response.data;
+        return decodedResponse['candidates'][0]['content']['parts'][0]['text'];
+      } else {
+        throw Exception(
+            'Failed to fetch response from Gemini API. Status code: ${response.statusCode}, body: ${response.data}');
+      }
+    } catch (e) {
+      throw Exception('Error requesting Gemini API: $e');
+    }
+  }
+}
+
+class OpenAIProvider extends AIProvider {
+  static const String apiUrl = "https://api.openai.com/v1/chat/completions";
+
+  final String apikey;
+  final String model;
+
+  OpenAIProvider({required this.apikey, required this.model});
+
+  @override
+  Future<String> request(String prompt) async {
+    final dio = Dio();
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer $apikey',
+    };
+    final data = {
+      "model": model,
+      "messages": [
+        {"role": "user", "content": prompt}
+      ]
+    };
+
+    try {
+      final response = await dio.post(
+        apiUrl,
+        options: Options(headers: headers),
+        data: jsonEncode(data),
+      );
+
+      if (response.statusCode == 200) {
+        final decodedResponse = response.data;
+        return decodedResponse['choices'][0]['message']['content'];
+      } else {
+        throw Exception(
+            'Failed to fetch response from OpenAI API. Status code: ${response.statusCode}, body: ${response.data}');
+      }
+    } catch (e) {
+      throw Exception('Error requesting OpenAI API: $e');
+    }
+  }
+}
