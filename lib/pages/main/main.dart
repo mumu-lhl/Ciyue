b2818bb8ff0351f11fc6693de46a82aed92c8838
Mumulhl <mumulhl.666@gmail.com>
2025-04-03 21:14:28 +0800

    feat: support navigation rail

--- lib/pages/main/main.dart
+++ lib/pages/main/main.dart
@@ -2,8 +2,15 @@ import "package:ciyue/pages/main/ai_translate_page.dart";
 import "package:ciyue/pages/main/home.dart";
 import "package:ciyue/pages/main/settings.dart";
 import "package:ciyue/pages/main/wordbook.dart";
-import "package:flutter/material.dart";
 import "package:ciyue/src/generated/i18n/app_localizations.dart";
+import "package:flutter/material.dart";
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
 
 class MainPage {
   static void Function(int)? _setScreenIndex;
@@ -11,13 +18,6 @@ class MainPage {
   static void Function(int) get setScreenIndex => _setScreenIndex!;
   static set setScreenIndex(void Function(int) callback) =>
       _setScreenIndex = callback;
-}
-
-class Home extends StatefulWidget {
-  const Home({super.key});
-
-  @override
-  State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
@@ -33,37 +33,74 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _pages,
-      ),
-      bottomNavigationBar: NavigationBar(
-        onDestinationSelected: (int index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        selectedIndex: _currentIndex,
-        destinations: buildDestinations(context),
-      ),
+      body: buildBody(),
+      bottomNavigationBar: buildNavigationBar(),
     );
   }
 
-  List<NavigationDestination> buildDestinations(BuildContext context) {
-    return <NavigationDestination>[
-      NavigationDestination(
-          icon: const Icon(Icons.home),
-          label: AppLocalizations.of(context)!.home),
-      NavigationDestination(
-          icon: const Icon(Icons.translate),
-          label: AppLocalizations.of(context)!.translate),
-      NavigationDestination(
-          icon: const Icon(Icons.book),
-          label: AppLocalizations.of(context)!.wordBook),
-      NavigationDestination(
-          icon: const Icon(Icons.settings),
-          label: AppLocalizations.of(context)!.settings),
+  Widget buildBody() {
+    return MediaQuery.of(context).size.width > 600
+        ? Row(
+            children: [
+              NavigationRail(
+                labelType: NavigationRailLabelType.all,
+                destinations: [
+                  for (final destination in buildCommonDestinations())
+                    NavigationRailDestination(
+                      icon: destination.$1,
+                      label: Text(destination.$2),
+                    )
+                ],
+                selectedIndex: _currentIndex,
+                onDestinationSelected: (int index) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+                leading: const SizedBox(),
+              ),
+              const VerticalDivider(thickness: 1, width: 1),
+              Expanded(
+                child: IndexedStack(
+                  index: _currentIndex,
+                  children: _pages,
+                ),
+              ),
+            ],
+          )
+        : IndexedStack(
+            index: _currentIndex,
+            children: _pages,
+          );
+  }
+
+  List<(Icon, String)> buildCommonDestinations() {
+    return [
+      (const Icon(Icons.home), AppLocalizations.of(context)!.home),
+      (const Icon(Icons.translate), AppLocalizations.of(context)!.translate),
+      (const Icon(Icons.book), AppLocalizations.of(context)!.wordBook),
+      (const Icon(Icons.settings), AppLocalizations.of(context)!.settings)
     ];
+  }
+
+  NavigationBar? buildNavigationBar() {
+    return MediaQuery.of(context).size.width < 600
+        ? NavigationBar(
+            onDestinationSelected: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            selectedIndex: _currentIndex,
+            destinations: [
+              for (final destination in buildCommonDestinations())
+                NavigationDestination(
+                  icon: destination.$1,
+                  label: destination.$2,
+                )
+            ],
+          )
+        : null;
   }
 
   @override
