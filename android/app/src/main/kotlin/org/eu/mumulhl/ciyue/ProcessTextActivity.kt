ff6672cc6a9d262db3581a9a2148ba9bab6267c0
Mumulhl <mumulhl.666@gmail.com>
2025-05-01 18:53:31 +0800

    feat: implement floating window intially (WIP)

--- android/app/src/main/kotlin/org/eu/mumulhl/ciyue/ProcessTextActivity.kt
+++ android/app/src/main/kotlin/org/eu/mumulhl/ciyue/ProcessTextActivity.kt
@@ -3,22 +3,31 @@ package org.eu.mumulhl.ciyue
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import io.flutter.embedding.android.FlutterActivity
 
 class ProcessTextActivity : Activity() {
+    companion object {
+        const val EXTRA_TEXT_TO_SHOW = "extra_text_to_show"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val text = intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)?.toString() ?: ""
         
-        val intent = Intent(this, MainActivity::class.java).apply {
-            action = Intent.ACTION_PROCESS_TEXT
-            putExtra(Intent.EXTRA_PROCESS_TEXT, text)
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+//        val intent = Intent(this, MainActivity::class.java).apply {
+//            action = Intent.ACTION_PROCESS_TEXT
+//            putExtra(Intent.EXTRA_PROCESS_TEXT, text)
+//            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+//            addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
+//        }
+//
+//        startActivity(intent)
+//        finish()
+
+        val serviceIntent = Intent(this, FloatingWindowService::class.java).apply {
+            putExtra(EXTRA_TEXT_TO_SHOW, text)
         }
-        
-        startActivity(intent)
+        startService(serviceIntent)
         finish()
     }
 } 
