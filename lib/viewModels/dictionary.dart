a6e9d50fca35d3be2422511c7c657feff0beeb3a
Mumulhl <mumulhl.666@gmail.com>
2025-05-17 18:21:50 +0800

    refactor(manage dictionaries): enhance dictionary management features and improve state handling

--- lib/viewModels/dictionary.dart
+++ lib/viewModels/dictionary.dart
@@ -6,31 +6,65 @@ class DictManagerModel extends ChangeNotifier {
   int groupId = dictManager.groupId;
   bool isEmpty = dictManager.isEmpty;
 
-  void checkIsEmpty() {
-    isEmpty = dictManager.isEmpty;
+  int state = 0;
+
+  bool checkIsEmpty() {
+    if (dictManager.isEmpty != isEmpty) {
+      isEmpty = dictManager.isEmpty;
+      notifyListeners();
+
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  Future<void> add(String path) async {
+    await dictManager.add(path);
+
+    update();
   }
 
   Future<void> close(int id) async {
     await dictManager.close(id);
-    checkIsEmpty();
-    notifyListeners();
+
+    state += 1;
+
+    if (!checkIsEmpty()) {
+      notifyListeners();
+    }
   }
 
   Future<void> setCurrentGroup(int id) async {
     await dictManager.setCurrentGroup(id);
     groupId = id;
-    checkIsEmpty();
-    notifyListeners();
+
+    state += 1;
+
+    if (!checkIsEmpty()) {
+      notifyListeners();
+    }
   }
 
   Future<void> updateDictIds() async {
     dictManager.dictIds = await dictGroupDao.getDictIds(dictManager.groupId);
-    checkIsEmpty();
-    notifyListeners();
+
+    state += 1;
+
+    if (!checkIsEmpty()) {
+      notifyListeners();
+    }
   }
 
   Future<void> updateGroupList() async {
     dictManager.groups = await dictGroupDao.getAllGroups();
+
+    state += 1;
+    notifyListeners();
+  }
+
+  void update() {
+    state += 1;
     notifyListeners();
   }
 }
