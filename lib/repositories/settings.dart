478be5676d7bd20ac558485cc93e75e273c78a4a
Mumulhl <mumulhl.666@gmail.com>
2025-08-01 22:54:31 +0800

    Default dictionary switch style is tag now

--- lib/repositories/settings.dart
+++ lib/repositories/settings.dart
@@ -70,7 +70,7 @@ class _Settings {
     final dictionarySwitchStyleString =
         prefs.getString("dictionarySwitchStyle");
     if (dictionarySwitchStyleString == null) {
-      dictionarySwitchStyle = DictionarySwitchStyle.expansion;
+      dictionarySwitchStyle = DictionarySwitchStyle.tag;
     } else {
       dictionarySwitchStyle =
           DictionarySwitchStyle.values.byName(dictionarySwitchStyleString);
