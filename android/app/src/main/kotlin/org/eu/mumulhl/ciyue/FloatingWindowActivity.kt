9f7cf127a4dbf4b75641a2b4831556c5f3c1448c
Mumulhl <mumulhl.666@gmail.com>
2025-08-02 21:52:22 +0800

    Click background to disable window

--- android/app/src/main/kotlin/org/eu/mumulhl/ciyue/FloatingWindowActivity.kt
+++ android/app/src/main/kotlin/org/eu/mumulhl/ciyue/FloatingWindowActivity.kt
@@ -1,5 +1,10 @@
 package org.eu.mumulhl.ciyue
 
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.MotionEvent
+import android.view.WindowManager
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 
@@ -8,8 +13,30 @@ class FloatingWindowActivity : FlutterActivity() {
         return io.flutter.embedding.engine.FlutterEngineCache.getInstance().get(FloatingWindowService.ENGINE_ID)
     }
 
-    // This is needed to ensure the transparent background is rendered correctly.
     override fun getTransparencyMode(): io.flutter.embedding.android.TransparencyMode {
         return io.flutter.embedding.android.TransparencyMode.transparent
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val rootView = window.decorView.rootView
+        rootView.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                finish()
+            }
+            true
+        }
+
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        val screenWidth = displayMetrics.widthPixels
+        val screenHeight = displayMetrics.heightPixels
+
+        val layoutParams = WindowManager.LayoutParams()
+        layoutParams.copyFrom(window.attributes)
+        layoutParams.width = (screenWidth * 0.8).toInt()
+        layoutParams.height = (screenHeight * 0.5).toInt()
+        window.attributes = layoutParams
     }
 }
