aea33f484fb1bea434f99b96921ae599192e6650
Mumulhl <mumulhl.666@gmail.com>
2025-08-06 11:33:36 +0800

    Fix properties view model dispose

--- lib/viewModels/dictionary_properties_view_model.dart
+++ lib/viewModels/dictionary_properties_view_model.dart
@@ -6,9 +6,25 @@ class DictionaryPropertiesViewModel extends ChangeNotifier {
   String _title = "";
   int _entriesTotal = 0;
 
+  bool _disposed = false;
+
   bool get isLoading => _isLoading;
   String get title => _title;
   int get entriesTotal => _entriesTotal;
+
+  @override
+  void dispose() {
+    _disposed = true;
+    super.dispose();
+  }
+
+  @override
+  void notifyListeners() {
+    if (_disposed) {
+      return;
+    }
+    super.notifyListeners();
+  }
 
   Future<void> fetchProperties(String path, int id) async {
     _isLoading = true;
